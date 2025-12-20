a210cd38df3785e79978d8015f3e9458c2eb8f26
chuntfeels-max <chuntfeels@gmail.com>
2025-12-20 19:25:04 +0800

    描述：v 1.5.6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 确保服务器端正确处理外部依赖
+      config.externals = config.externals || [];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
